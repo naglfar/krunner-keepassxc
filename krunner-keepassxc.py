a3057e77aef610b306a8472e3e690cd1e8d9f221
naglfar <stefan@mtztech.info>
2020-05-16 13:17:31 +0200

    use KeepassPasswords module in krunner

--- krunner-keepassxc.py
+++ krunner-keepassxc.py
@@ -4,14 +4,19 @@ from gi.repository import GLib
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
 
+from KeepassPasswords import KeepassPasswords
 
 objpath="/krunner"
 iface="org.kde.krunner1"
 
+
 class Runner(dbus.service.Object):
+	
+	kp = None
 	
 	def __init__(self):
 		dbus.service.Object.__init__(self, dbus.service.BusName("de.naglfar.krunner-keepassxc", dbus.SessionBus()), objpath)
+		self.kp = KeepassPasswords()
 
 	@dbus.service.method(iface, out_signature='a(sss)')
 	def Actions(self, msg):
@@ -20,20 +25,33 @@ class Runner(dbus.service.Object):
 
 	@dbus.service.method(iface, in_signature='s', out_signature='a(sssida{sv})')
 	def Match(self, query):
-			
-		#		data, display text, icon, type (Plasma::QueryType), relevance (0-1), properties (subtext, category and urls)
-		return [(query,"Copy to clipboard","object-unlocked",100,0.9,{})]
+		
+		if len(query) > 2:
+			items = [i for i in self.kp.labels if query.lower() in i.lower()]
+			if len(items) > 0:
+				# if multiple items found, see if we can get a direct hit, else return the first one
+				if len(items) > 1:
+					directHit = [i for i in items if query.lower() == i.lower()]
+					if len(directHit) > 0:
+						items = directHit
+
+				item = items[0]
+					
+				#		data, display text, icon, type (Plasma::QueryType), relevance (0-1), properties (subtext, category and urls)
+				return [(item,"Copy to clipboard: " + item,"object-unlocked",100,1,{})]
+		
+		return []
+		
 
 	@dbus.service.method(iface, in_signature='ss',)
 	def Run(self, matchId, actionId):
 
-		result = subprocess.run("secret-tool lookup Title " + matchId, shell=True, stdout=subprocess.PIPE)
-		for line in result.stdout.decode('utf-8').split('\n'):
-			if line:
-				#print(line)
-				line = line.replace('"','\\"')
-				line = line.replace("'","\\'")
-				subprocess.run('printf "'+line+'" | xsel -b', shell=True, stdout=subprocess.PIPE)
+		if len(matchId) > 0:
+			secret = self.kp.getSecret(matchId)
+			if secret:
+				secret = secret.replace('"','\\"')
+				secret = secret.replace("'","\\'")
+				subprocess.run('printf "'+secret+'" | xsel -b', shell=True, stdout=subprocess.PIPE)
 				
 		return
 

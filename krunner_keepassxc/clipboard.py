725db0bcec5e1e9ac6fa9f254aa3495130e78aab
naglfar <stefan@mtztech.info>
2022-04-26 19:19:25 +0200

    feat: remove xsel/xclip dependency by using klipper dbus service

--- krunner_keepassxc/clipboard.py
+++ krunner_keepassxc/clipboard.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+import dbus
 
 from typing import Tuple, Callable
 
@@ -13,12 +14,20 @@ class Clipboard:
 	def check_executables(self):
 		copy, paste = None, None
 		self.can_clip = True
-		if self._executable_exists("xclip"):
-			copy, paste = self.init_xclip_clipboard()
-		elif self._executable_exists("xsel"):
-			copy, paste = self.init_xsel_clipboard()
-		else:
-			self.can_clip = False
+		self.bus = dbus.SessionBus()
+
+		try:
+			self.klipper = self.bus.get_object('org.kde.klipper', '/klipper')
+			copy = self.init_klipper_clipboard()
+
+		except dbus.exceptions.DBusException as e:
+
+			if self._executable_exists("xclip"):
+				copy, paste = self.init_xclip_clipboard()
+			elif self._executable_exists("xsel"):
+				copy, paste = self.init_xsel_clipboard()
+			else:
+				self.can_clip = False
 
 		# FIXME: mypy issue #2427
 		if copy:
@@ -35,6 +44,14 @@ class Clipboard:
 	def _executable_exists(self, name: str):
 		return subprocess.call(['which', name], stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
+	def init_klipper_clipboard(self) -> Callable:
+
+		def copy_klipper(text: str, primary: bool=False):
+			klipper = dbus.Interface(self.klipper, dbus_interface='org.kde.klipper.klipper')
+			klipper.setClipboardContents(text)
+
+		return copy_klipper
+
 	def init_xclip_clipboard(self) -> Tuple[Callable,Callable]:
 		DEFAULT_SELECTION: str = 'c'
 		PRIMARY_SELECTION: str = 'p'
@@ -45,7 +62,7 @@ class Clipboard:
 				selection = PRIMARY_SELECTION
 			p = subprocess.Popen(['xclip', '-selection', selection], stdin=subprocess.PIPE, close_fds=True)
 			_stdout, stderr = p.communicate(input=text.encode('utf-8'))
-			if stderr: 
+			if stderr:
 				print(stderr, file=sys.stderr)
 
 		def paste_xclip(primary: bool=False) -> str:
@@ -72,7 +89,7 @@ class Clipboard:
 				selection_flag = PRIMARY_SELECTION
 			p = subprocess.Popen(['xsel', selection_flag, '-i'], stdin=subprocess.PIPE, close_fds=True)
 			_stdout, stderr = p.communicate(input=text.encode('utf-8'))
-			if stderr: 
+			if stderr:
 				print(stderr, file=sys.stderr)
 
 		def paste_xsel(primary: bool=False):
